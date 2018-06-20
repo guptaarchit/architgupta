d9f3f7b55b9baa0e069c2e5639b7c3bdab594995
Archit Gupta <guptaarchit21@gmail.com>
2018-06-20 23:05:49 +0530

    First commit

--- architgupta/settings.py
+++ architgupta/settings.py
@@ -130,10 +130,4 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+django_heroku.settings(locals())
