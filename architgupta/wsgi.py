e907b8a6f919a110471a34edc45695fc2bd9261c
Archit Gupta <guptaarchit21@gmail.com>
2018-06-20 22:59:20 +0530

    First commit

--- architgupta/wsgi.py
+++ architgupta/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "architgupta.settings")
+
+#application = get_wsgi_application()
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "architgupta.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
